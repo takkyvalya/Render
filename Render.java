a8b113facfc4bd88b1fefa7a99d93bf92827a79f
takkyvalya <65748453+takkyvalya@users.noreply.github.com>
2021-01-19 11:33:38 +0300

    Update Render.java

--- Render.java
+++ Render.java
@@ -13,6 +13,29 @@ public class Render {
             }
         }
     }
+    public static void renderTriangle(BufferedImage img, int x1, int y1, int x2, int y2, int x3, int y3, Color color, Color color2) {
+
+        for (int i = Math.min(x1, Math.min(x2, x3)); i <= Math.max(x1, Math.max(x2, x3)); i++) {
+            for (int j = Math.min(y1, Math.min(y2, y3)); j <= Math.max(y1, Math.max(y2, y3)); j++) {
+                double alpha=(double) ((j-x1) * (y3-y1) - (x3-x1)* (j-y1)) / (double) ((x2-x1) * (y3-y1)-(x3-x1)*(y2-y1));
+                double beta=(double) ((x2-x1) * (j-y1) - (y2-y1) * (i-x1)) / (double) ((x2-x1) * (y3-y1)-(x3-x1)*(y2-y1));
+
+                    //System.out.println(alpha + " " + beta + " ");
+
+                //System.out.println(alpha + " " + beta + " ");
+                //System.out.println(x2 * y3 - x3 * y2);
+                if(alpha>0 && beta>0 && beta+alpha<=1){
+                    img.setRGB(i,j,color2.getRGB());
+                }
+            }
+        }
+        img.setRGB(x1, y1, color.getRGB());
+        img.setRGB(x2, y2, color.getRGB());
+        img.setRGB(x3, y3, color.getRGB());
+
+    }
+
+    
 
     public static void renderLine(BufferedImage img, int x1, int y1, int x2, int y2, Color color) {
         img.setRGB(x1,y1,color.getRGB());

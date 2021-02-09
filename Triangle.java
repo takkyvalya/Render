a25c986468e214ec6ddef85a29a1b0695ffc97e0
takkyvalya <65748453+takkyvalya@users.noreply.github.com>
2021-02-09 11:27:20 +0300

    Add files via upload

---
+++ Triangle.java
@@ -0,0 +1,18 @@
+public class Triangle {
+    double x1;
+    double y1;
+    double x2;
+    double y2;
+    double x3;
+    double y3;
+
+
+    public Triangle(double x1, double y1, double x2, double y2, double x3, double y3) {
+        this.x1 = x1;
+        this.y1 = y1;
+        this.x2 = x2;
+        this.y2 = y2;
+        this.x3 = x3;
+        this.y3 = y3;
+    }
+}
---
+++ Vector3.java
@@ -0,0 +1,14 @@
+public class Vector3 {
+    double xV;
+    double yV;
+    double zV;
+
+    public Vector3(double xV, double yV, double zV) {
+        this.xV = xV;
+        this.yV = yV;
+        this.zV = zV;
+    }
+
+
+
+}
